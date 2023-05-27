63c114227db5e0c340f05c5f089d400dcae8ff2a
arkosh <trukan24@gmail.com>
2023-05-27 14:12:34 +0300

    :recycle: del list

--- internal/client/grpc/auth/client.go
+++ internal/client/grpc/auth/client.go
@@ -5,13 +5,11 @@ import (
 
 	accessV1 "github.com/Arkosh744/auth-service-api/pkg/access_v1"
 	userV1 "github.com/Arkosh744/auth-service-api/pkg/user_v1"
-	"github.com/golang/protobuf/ptypes/empty"
 )
 
 var _ Client = (*client)(nil)
 
 type Client interface {
-	List(ctx context.Context) ([]*userV1.UserInfo, error)
 	Check(ctx context.Context, endpoint string) error
 }
 
@@ -25,15 +23,6 @@ func NewClient(c userV1.UserClient, a accessV1.AccessV1Client) *client {
 		userClient:   c,
 		accessClient: a,
 	}
-}
-
-func (c *client) List(ctx context.Context) ([]*userV1.UserInfo, error) {
-	res, err := c.userClient.List(ctx, &empty.Empty{})
-	if err != nil {
-		return nil, err
-	}
-
-	return res.GetUsers(), nil
 }
 
 func (c *client) Check(ctx context.Context, endpoint string) error {
